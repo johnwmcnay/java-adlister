a33cd48dc66556a346d2871270df49533983a7c6
John McNay <john.w.mcnay@gmail.com>
2020-11-05 14:25:44 -0600

    updated with preferred solution

--- src/main/java/ViewAds.java
+++ src/main/java/ViewAds.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ViewAds", urlPatterns="/ads")
 public class ViewAds extends HttpServlet {
@@ -12,8 +13,9 @@ public class ViewAds extends HttpServlet {
 //    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ListAdsDao ads = new ListAdsDao();
-        request.setAttribute("ads", ads.all());
+        Ads adsDao = DaoFactory.getAdsDao();
+        List<Ad> ads = adsDao.all();
+        request.setAttribute("ads", ads);
         request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
 }
