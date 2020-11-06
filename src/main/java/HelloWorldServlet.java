351bfaa96980f412c7e9e4f20f631f3aedd4afcc
John McNay <john.w.mcnay@gmail.com>
2020-11-06 11:24:33 -0600

    sessions & cookies completed

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,12 +1,22 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("user") != null && session.getAttribute("user").equals(true)) {
+            response.sendRedirect("/profile");
+            return;
+        } else {
+            session.setAttribute("user", false);
+        }
+        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 }
