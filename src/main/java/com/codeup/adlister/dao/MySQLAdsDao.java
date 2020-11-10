42341ac91d77858423a2756ac6f7aad2fd410555
John McNay <john.w.mcnay@gmail.com>
2020-11-10 11:17:14 -0600

    refactored SQL insert code to prevent injection

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -41,9 +41,11 @@ public class MySQLAdsDao implements Ads {
     @Override
     public Long insert(Ad ad) {
         try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+
+            PreparedStatement stmt = createInsertQuery(ad);
+            stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
+
             rs.next();
             return rs.getLong(1);
         } catch (SQLException e) {
@@ -51,11 +53,14 @@ public class MySQLAdsDao implements Ads {
         }
     }
 
-    private String createInsertQuery(Ad ad) {
-        return "INSERT INTO ads(user_id, title, description) VALUES "
-            + "(" + ad.getUserId() + ", "
-            + "'" + ad.getTitle() +"', "
-            + "'" + ad.getDescription() + "')";
+    private PreparedStatement createInsertQuery(Ad ad) throws SQLException {
+        String sql = "INSERT INTO ads (user_id, title, description) VALUES (?, ?, ?)";
+        PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        stmt.setLong(1, ad.getUserId());
+        stmt.setString(2, ad.getTitle());
+        stmt.setString(3, ad.getDescription());
+
+        return stmt;
     }
 
     private Ad extractAd(ResultSet rs) throws SQLException {
